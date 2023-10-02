06239c489ecf8bb8c1e3fcf4ca299885c68579cf
BrantLauro <brantlauro@gmail.com>
2023-10-01 22:41:12 -0300

    ex004 added

---
+++ ex004/main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Double Analises</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <br>
+    <?php $num = "" ?>
+    <section>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <h1 style="text-align:center;">Double Analises!</h1>
+            <label for="num">Type a double number:</label>
+            <input type="number" name="num" step="0.001">
+            <input type="submit" value="Submit">
+        </form>
+        
+        <?php
+            $num = $_POST["num"] ?? "";
+            if($num != ""){
+                $trunc = intval($num);
+                $fp = $num - $trunc; ?>
+                <p style="text-align:center;">
+                    The typed number was: <strong><?=number_format($num, 3)?></strong><br>
+                    The int part is: <strong> <?=number_format($trunc, 0)?></strong><br>
+                    The double part is: <strong> <?=number_format($fp, 3)?></strong>
+                </p>
+        <?php } ?>
+    </section>
+    <footer>
+        <p>&copy; LauroBrant</p>
+    </footer>
+</body>
+</html>
