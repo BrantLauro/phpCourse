b530c7864853c32bc1fd74b6572592016748b186
BrantLauro <brantlauro@gmail.com>
2023-10-01 22:41:00 -0300

    ex002 added

---
+++ ex002/main.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aleatory Numbers</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <h1 style="text-align:center;">Randomize:</h1>
+            <p>Press Generate to randomize the 6 numbers from <strong>Mega Sena</strong></p>
+            <?php if($_SERVER['REQUEST_METHOD'] === 'POST') { ?>
+                <p>Result: <strong><?php randms()?></strong><br>
+            <?php } ?>
+            <input type="submit" value="Generate">
+        </form>
+    </section>
+    <footer>
+        <p>&copy; LauroBrant</p>
+    </footer>
+</body>
+<?php 
+    function randms() {
+        for($i = 0; $i < 6; $i++){
+            echo rand(1, 60);
+            if($i != 5) echo "</strong>, <strong>";
+        }
+    }
+?>
+</html>
