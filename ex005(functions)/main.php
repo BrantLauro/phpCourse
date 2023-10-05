58e0306cd32f32891133c77e6771271b602dd67f
BrantLauro <brantlauro@gmail.com>
2023-10-04 23:48:38 -0300

    ex005 finished

--- ex005(functions)/main.php
+++ ex005(functions)/main.php
@@ -7,10 +7,33 @@
     <link rel="stylesheet" href=".\style.css">
     <?php 
         #include ou require, require interrompe a execução
-        include "funcions.php"; 
+        include "functions.php"; 
     ?>
 </head>
 <body>
-    
+    <br>
+    <section>
+        <?php $n1 = ""; $n2 = ""; ?>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <h1 style="text-align:center;">Functions Tester!</h1>
+            <label for="reais">Type a number:</label>
+            <input type="number" name="n1">
+            <label for="reais">Type another number:</label>
+            <input type="number" name="n2">
+            <input type="submit" value="Submit">
+        </form>
+        <?php
+            $n1 = $_POST["n1"] ?? "";
+            $n2 =  $_POST["n2"] ?? "";
+            if($n1 != "" && $n2 != "") { ?>
+                <p>First Function: <?="$n1 + $n2 = ". sum($n1, $n2)?><br></p>
+                <p>Second Function: <?="$n1 + $n2 + ($n1 + 1) + ($n2 + 2) = ". sumMult($n1, $n2, $n1 + 1, $n2 + 2)?><br></p>
+                <p>Third Function: <?="$n1 + 10 = ". add($n1)?><br></p>
+                <p>$n1 final value: <?=$n1?></p>
+        <?php }?>
+    </section>
+    <footer>
+        <p>&copy; LauroBrant</p>
+    </footer>
 </body>
 </html>
